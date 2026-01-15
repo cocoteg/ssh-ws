46b6a413e78ca7d258ae8acdcb864505f0ab49d6
cocoteg <94770769+cocoteg@users.noreply.github.com>
2026-01-15 22:22:06 +0700

    Create domain.sh

---
+++ menu/domain.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clear
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "Domain anda saat ini:"
+echo -e "$(cat /root/.domain)"
+echo ""
+read -rp "Domain/Host: " -e host
+echo ""
+if [ -z $host ]; then
+echo "DONE CHANGE DOMAIN"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
+else
+echo "$host" > /root/.domain
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+read -p "Input to back to menu"
+menu
+fi
